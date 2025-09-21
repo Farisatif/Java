308b03be3c79846afd130e8c6e24492b7900d59d
彡ℤ𝕖𝕣𝕖𝕗 <146383261+Farisatif@users.noreply.github.com>
2025-09-21 11:17:47 +0300

    Delete demo5 directory

--- demo5/src/main/java/com/example/demo5/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo5;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        com.example.demo5.Application.launch(HelloApplication.class, args);
-    }
-}
